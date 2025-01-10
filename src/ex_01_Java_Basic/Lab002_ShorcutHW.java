5547c08aa0b2395c922be6ee5e5fc127295d17fe
Deepa Divate <ddd.qa0710@gmail.com>
2025-01-10 20:01:24 +0530

    5th JAN

--- src/ex_01_Java_Basic/Lab002_ShorcutHW.java
+++ src/ex_01_Java_Basic/Lab002_ShorcutHW.java
@@ -1,5 +1,6 @@
 package ex_01_Java_Basic;
 
 public class Lab002_ShorcutHW {
-    public ublic static void main(String[] args) {
+    public static void main(String[] args) {
     }
+}
--- src/ex_01_Java_Basic/Lab004_Comments.java
+++ src/ex_01_Java_Basic/Lab004_Comments.java
@@ -1,8 +1,11 @@
 package ex_01_Java_Basic;
 
 public class Lab004_Comments {
-    // System.out.println("Hello");
-    //This is single line comment line
-    // This is a Comment, It will be ignored by JVM/ Compiler
+
+    public static void main() {
+        // System.out.println("Hello");
+        //This is single line comment line
+        // This is a Comment, It will be ignored by JVM/ Compiler
         System.out.println("Hi");
+    }
 }
