e043ff4d5886a33c38cdc1a848239ed7ad04dd6f
Deepa Divate <ddd.qa0710@gmail.com>
2025-01-10 19:59:24 +0530

    5th JAN

--- src/ex_01_Java_Basic/Lab001_HelloWordProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWordProgram.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basic;
 
-public class HelloWordProgram {
+public class Lab001_HelloWordProgram {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
--- src/ex_01_Java_Basic/Lab004_Comments.java
+++ src/ex_01_Java_Basic/Lab004_Comments.java
@@ -3,4 +3,6 @@ package ex_01_Java_Basic;
 public class Lab004_Comments {
     // System.out.println("Hello");
     //This is single line comment line
+    // This is a Comment, It will be ignored by JVM/ Compiler
+        System.out.println("Hi");
 }
