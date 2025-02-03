b83323f99ee6e4c0c08dff33ce943947918865f2
Deepa Divate <ddd.qa0710@gmail.com>
2025-02-03 20:42:00 +0530

    5th JAN

---
+++ src/ex_01_Java_Basic/Lab013_Variables.java
@@ -0,0 +1,42 @@
+package ex_01_Java_Basic;
+
+public class Lab013_Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int a = 10;
+        // data type variable_name =  variable_value
+        // GST 18%
+
+        int age = 65;
+        age = 89;
+        System.out.println(age); // print updated value of age variable
+
+
+        int age1 = 65;
+        age1 = 65 + 1;
+        System.out.println(age1);
+
+
+        int age2 = 76;
+        age = age2+1;
+        age2 = 76;
+        System.out.println(age2);
+
+
+        //        int _ = 65;
+        int $ = 65;
+        int yest = 65;
+        // int -> primitive - data type - numeric
+        // it stores the whole number - without any decimal point
+        // age -> variable name or identifier
+        int this_is_a_vari_long_name_in_the_class_hello_$_akakak = 89;
+
+        // =  operator Assignment ->
+        // 65 ->  literal (int) value
+
+
+
+    }
+
+}
